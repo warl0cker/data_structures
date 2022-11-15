832bb7d941bafe673b73a191f030c1bd1efcb53d
warl0cker <117690765+warl0cker@users.noreply.github.com>
2022-11-15 20:22:22 +0200

    Update Exercitii stive si cozi - Laborator 7.c

--- Exercitii stive si cozi - Laborator 7.c
+++ Exercitii stive si cozi - Laborator 7.c
@@ -1,31 +1,31 @@
-// 1. Scrieți o secventa de cod care citește un sir de caractere și le imprimă      în ordine invers, folosind o stivă.
+// 1. Scrieți o secventa de cod care citește un numar intreg și afiseaza cifrele numarului în ordine inversa, folosind o stivă.
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 typedef struct node {
-    char chatacter;
+    int number;
     struct node *next;
 } node;
 
-void push(node **list, char chatacter) {
-    node *ch = malloc(sizeof(node));
-    ch->chatacter = chatacter;
-    ch->next = (*list);
-    (*list) = ch;
+void push(node **list, int number) {
+    node *nr = malloc(sizeof(node));
+    nr->number = number;
+    nr->next = (*list);
+    (*list) = nr;
 }
 
 int main() {
-    char *string;
-    scanf("%ms", &string);
-    int length = strlen(string);
+    int nr, rest = 0;
+    scanf("%d", &nr);
     node *lista = NULL;
-    for (int i = 0; i < length; i++) {
-        push(&lista, string[i]);
-	}
+    while (nr != 0) {
+        rest = nr % 10;
+        nr /= 10;
+        push(&lista, rest);
+    }
     for (node *tmp = lista; tmp != NULL; tmp = tmp->next) {
-        printf("%c ", tmp->chatacter);
-	}
+        printf("%c ", tmp->number);
+    }
     return 0;
 }
 
@@ -38,7 +38,7 @@ typedef struct node {
     struct node *next;
 }node;
 
-void push(node **list, char character){
+void push(node **list, char character) {
     node *ch = malloc(sizeof(node));
     ch->character = character;
     ch->next = (*list);
@@ -63,19 +63,18 @@ void deleteNode(node **list, char character) {
 }
 
 int main() {
-    int top = 0;
-    char *expression;
-    scanf("%ms", &expression);
+    char expression[100];
+    scanf("%99s", expression);
     node *lista = NULL;
     for (int i = 0; expression[i] != '\0'; i++) {
         if (expression[i] == '(') push(&lista, expression[i]);
         if (expression[i] == ')') deleteNode(&lista, expression[i]);
     }
-    (lista == NULL) ? printf("expression is valid\n") : printf("expression is invalid\n");
+    (lista == NULL) ? printf("expresia este valida\n") : printf("expresia este invalida\n");
     return 0;
 }
 
-// 3. Scrieți o secventa de cod care citește un număr întreg pozitiv și imprimă reprezentarea binara a acelui număr întreg. Sugestie: împărțiți numărul întreg la 2
+// 3. Scrieți o secventa de cod care citește un număr întreg pozitiv și imprimă reprezentarea in baza 8 a acelui număr întreg.
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -94,11 +93,11 @@ void push(node **list, int number) {
 int main() {
     int nr;
     scanf("%d", &nr);
-    if (nr < 0) nr *= -1;
+    
     node *lista = NULL;
     while (nr != 0) {
-        push(&lista, (nr % 2));
-        nr /= 2;
+        push(&lista, (nr % 8));
+        nr /= 8;
     }
     for (node *tmp = lista; tmp != NULL; tmp = tmp->next) {
         printf("%d", tmp->number);
@@ -108,35 +107,10 @@ int main() {
 
 // 4. Scrieti o secventa de cod pentru generarea primelor 50 de numere ale seriei de numere:  S1 = N, S2 = S1 + 1, S3 = 2*S1 + 1, S4 = S1 + 2, S5 = S2 + 1, S6 = 2*S2 + 1, S7 = S2 + 2, …
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node {
-    int number;
-    struct node *next;
-} node;
-
-void append(node **list, int number) {
-    node *nr = malloc(sizeof(node));
-    node *last = (*list);
-    nr->number = number;
-    nr->next = NULL;
-    if ((*list) == NULL) {
-        (*list) = nr;
-        return;
-    }
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    last->next = nr;
-    return;
-}
 
 int main() {
-    int n, tmp, sum[50], cnt = 1, s = 0;
-    scanf("%d", &n);
-    tmp = n;
-    node *lista = NULL;
-    append(&lista, n);
+    int tmp, sum[50], cnt = 1, s = 0;
+    scanf("%d", &tmp);
     for (int i = 0; i < 50; i++) {
         if (cnt == 4) {
             cnt = 1;
@@ -145,11 +119,11 @@ int main() {
         if (cnt == 1) sum[i] = tmp + 1;
         if (cnt == 2) sum[i] = 2 * tmp + 1;
         if (cnt == 3) sum[i] = tmp + 2;
-        append(&lista, sum[i]);
         ++cnt;
     }
-    for (node *temp = lista; temp != NULL; temp = temp->next) {
-        printf("%d, ", temp->number);
+    for (int i = 0; i < 50; i++) {
+        printf("%d ", sum[i]);
+        if (i != 49) printf(", ");
     }
     return 0;
 }
@@ -181,7 +155,7 @@ int main() {
         node *lista = NULL;
         push(&lista, b);
         for (int i = 0; i < b / 2; i++) {
-            if      (b / 2 >= a) push(&lista, (b /= 2));
+            if (b / 2 >= a) push(&lista, (b /= 2));
             else if (b - 2 >= a) push(&lista, (b -= 2));
             else if (b - 1 >= a) push(&lista, (b -= 1));
             else break;
