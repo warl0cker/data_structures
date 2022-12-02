efbaae7a386d73f37f41a611e8f397220c0208e4
Bogdan Radu <117690765+warl0cker@users.noreply.github.com>
2022-12-02 12:12:47 +0200

    Add files via upload

--- Exercitii arbori binari de cautare - Laborator 9.c
+++ Exercitii arbori binari de cautare - Laborator 9.c
@@ -1,67 +1,72 @@
-// Exercitiul 9.1. Introduceți elemente cu următoarele chei (în ordinea dată) într-un ABC inițial NULL: 30 , 40 , 24 , 58 , 48 , 26 , 11 , 13 . Reprezentati arborele după oricare două inserții. Scrieti parcurgerile InOrdine, PreOrdine si PostOrdine pentru arborele generat.
-#include <stdio.h>
-#include <stdlib.h>
+**************************************************************************************
+Exercitiul 9.1. 
+Introduceți elemente cu următoarele chei (în ordinea dată) într-un ABC inițial NULL:
+30, 40, 24, 58, 48, 26, 11, 13. Reprezentati arborele după oricare două inserții.
+Scrieti parcurgerile InOrdine, PreOrdine si PostOrdine pentru arborele generat.
 
-typedef struct node {
-    int key;
-    struct node *left, *right;
-} node;
+                30
+                / \
+               /   \
+              24   40
+             / \     \
+            /   \    58
+           11   26   /
+            \       48
+            13
 
-node* newNode(int item) {
-    node* temp = malloc(sizeof(node));
-    temp->key = item;
-    temp->left = temp->right = NULL;
-    return temp;
-}
-node* insert(node* node, int key) {
-    if (node == NULL)
-        return newNode(key);
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-    return node;
-}
-void inorder(node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->key);
-        inorder(root->right);
-    }
-}
-void preorder(node* root) {
-    if (root != NULL) {
-        printf("%d ", root->key);
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
-void postorder(node* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->key);
-    }
-}
+  InOrder: 11 13 24 26 30 40 48 58
+ PreOrder: 30 24 11 13 26 40 58 48
+PostOrder: 13 11 26 24 48 58 40 30
 
-int main() {
-    node* root = NULL;
-    root = insert(root, 30);
-    insert(root, 40);
-    insert(root, 24);
-    insert(root, 58);
-    insert(root, 48);
-    insert(root, 26);
-    insert(root, 11);
-    insert(root, 13);
-    printf("InOrdine:\n");
-    inorder(root);
-    printf("\n\n");
-    printf("PreOrdine:\n");
-    preorder(root);
-    printf("\n\n");
-    printf("PostOrdine:\n");
-    postorder(root);
-    printf("\n\n");
-    return 0;
-}
+**************************************************************************************
+Exercitiul 9.2. 
+Având în vedere succesiunea preordine a unui ABC  {13,5,3,2,11,7,19,23}, reprezentati
+acest ABC și determinați dacă  este același cu cel descris de {2,3, 7,11,5,23,19,13}.
+
+                  13
+                  / \
+                 /   \
+                5    19
+               / \     \
+              /   \    23
+             3    11
+            /     /
+           2     7
+
+ PreOrder: 13 5 3 2 11 7 19 23 |
+                               |  => este acelasi.
+PostOrder: 2 3 7 11 5 23 19 13 |
+
+**************************************************************************************
+Exercitiul 9.3.
+a. Sa se insereze intr-un arbore binar de cautare, initial vid, urmatoarele chei, in
+ordinea data (reprezentati prin desen): 20, 5, 40, 1, 23, 15, 7, 6, 9, 0, 33, 20, 3,
+10, 5, 6, 26, 4, 41.
+b. Scrieti parcurgerile arborelui binar generat: PreOrdine, InOrdine, PostOrdine.
+c. Sa se stearga succesiv nodurile avand cheile: 10, 15 si 7.
+
+                        20
+                        / \
+                       /   \
+                      /     \
+                     /       \
+                    /         \
+                   5          40
+                  / \         / \
+                 /   \       /   \
+                /     \     23   41
+               /       \     \
+              /         \    33
+             1          15   /
+            / \         /   26
+           0   3       7
+                \     / \
+                 4   6   9
+                          \
+                          10
+
+  InOrder: 0 1 3 4 5 6 7 9 10 15 20 23 26 33 40 41
+ PreOrder: 20 5 1 0 3 4 15 7 6 9 10 40 23 33 26 41
+PostOrder: 0 4 3 1 6 10 9 7 15 5 26 33 23 41 40 20
+
+**************************************************************************************
