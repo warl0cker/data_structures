7d47830202f4623d4efa71f79e7e9dfbc768b66e
Bogdan Radu <117690765+warl0cker@users.noreply.github.com>
2022-12-27 12:37:54 +0200

    Add files via upload

---
+++ laboratoare/Laborator 13 - Exercitii cod huffman.c
@@ -0,0 +1,46 @@
+**************************************************************************************
+Exercitiul 13.1
+Construiti arborele binar asociat codului Huffman pentru textul: text de comprimat
+
+               __________15__________
+              |                      |
+       _______8_______             __7__
+      |               |           |     |
+   ___4___         ___4___       _4_    3
+  |       |       |       |     |   |   t
+ _2_     _2_     _2_     _2_    2   2
+|   |   |   |   |   |   |   |   e   m
+1   1   1   1   1   1   1   1
+x   d   c   o   p   r   i   a
+
+**************************************************************************************
+Exercitiul 13.2
+Pentru  arborele Huffman de mai jos:
+Determinati forma decomprimata a secventei: 11000101001000101100111001100100100011
+
+                       ____________________24___________________
+                      |                                         |
+       _______________16_____________                 __________8__________
+      |                              |               |                     |
+ _____8_____                    _____8_____     _____4_____           _____4_____
+|           |                  |           |   |           |         |           |
+4       ____4____          ____4____       4   2        ___2___   ___2___     ___2___
+_      |         |        |         |      T   A       |       | |       |   |       |
+       2     ____2____    2     ____2____              1       1 1       1   1       1
+       E    |         |   M    |         |             D       N C       L   U       X
+            1         1        1         1
+            I         R        O         P
+
+Forma decomprimata a secventei este: COMPRIMAT
+
+**************************************************************************************
+Exercitiul 13.3
+Fisierul date.in contine un text format numai din literele mari ale alfabetului latin.
+1. Scrieti un program C prin care caculati frecventa de aparitie a fiecarui caracter
+din text continut de fisier.
+
+**************************************************************************************
+Exercitiul 13.4
+Construiti apoi codul Huffman si arborele asociat acestuia.
+
+**************************************************************************************
